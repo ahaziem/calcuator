8e4eb7bd8bd15072c288d775b0511327b281ab69
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-03-29 16:49:51 +0100

    made changes

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById(number).addEventListener("click", () => console.log ('clicked!'));
+const numberbuttons = document.getElementsByClassName('number'); for (const button of buttons) {
+    button.addEventListener ('click', () => console.log ('clicked!')); }
