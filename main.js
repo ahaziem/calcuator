dceb8b3c429a1773749d812119c4a97ca31f715d
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-03-30 16:11:44 +0100

    made changes

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 // select the button in the caluator
-const numberKeys = document.querySelectorAll(".number");
+const numberKeys = document.querySelectorAll(".number && .number-decimal");
 const actionKey = document.querySelectorAll(".button-action");
 const clearaction = document.querySelectorAll(".button-action-clear");
 console.log(numberKeys)
 console.log("hello")
-//Make it interactive so does 
 
+//Make it interactive for each list of the numbers in the calculator
 // document.getElementById(number).addEventListener("click", () => console.log ('clicked!'));
-numberKeys.forEach( element => {
+numberKeys.forEach(element => {
+    element.addEventListener("click", (e) => {
+        if (e.target.matches('button')) {
+    }
+})
+// conduct the action of pressing the keys
+const buttonkey = e.target
+const actionbutton = buttonkey.dataset.actionbutton {
+    if (actionbutton = null) {
+        console.log(number-key)
+    }
+     if (actionbutton === "button-action') {
+     )
+     { console.log (operations-key)}
 
-    element.addEventListener( "click", (e) => {
-        console.log( e.target );
-    });
+        if ( actionbutton === "button-action-clear" )
+        {
+            console.log (clear-key)
+        }
 
-});
+        if (actionbutton === "number-decimal") {
+            console.log (demical-key)
+        }
