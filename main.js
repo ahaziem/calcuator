8acd84265c44f23622a064843363d17091a7f9c3
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-03-30 16:48:45 +0100

    change made in JS

--- main.js
+++ main.js
@@ -1,32 +1,44 @@
 // select the button in the caluator
-const numberKeys = document.querySelectorAll(".number && .number-decimal");
+const numberKeys = document.querySelectorAll(".number");
+const demicalkey = document.querySelectorAll(".number-decimal");
 const actionKey = document.querySelectorAll(".button-action");
 const clearaction = document.querySelectorAll(".button-action-clear");
-console.log(numberKeys)
-console.log("hello")
+// console.log(numberKeys)
+
 
 //Make it interactive for each list of the numbers in the calculator
 // document.getElementById(number).addEventListener("click", () => console.log ('clicked!'));
 numberKeys.forEach(element => {
     element.addEventListener("click", (e) => {
-        if (e.target.matches('button')) {
-    }
+        if (e.target.matches('button')) { 
+        }
+        console.log(e.target.innerHTML)
+    })
 })
-// conduct the action of pressing the keys
-const buttonkey = e.target
-const actionbutton = buttonkey.dataset.actionbutton {
-    if (actionbutton = null) {
-        console.log(number-key)
-    }
-     if (actionbutton === "button-action') {
-     )
-     { console.log (operations-key)}
 
-        if ( actionbutton === "button-action-clear" )
-        {
-            console.log (clear-key)
+demicalkey.forEach(element => {
+    element.addEventListener("click", (e) => {
+        if (e.target.matches('button')) { 
         }
+        console.log(demical-key)
+    })
+})
 
-        if (actionbutton === "number-decimal") {
-            console.log (demical-key)
+actionKey.forEach(element => {
+    element.addEventListener("click", (e) => {
+        if (e.target.matches('button')) { 
         }
+        console.log(action-key)
+    })
+})
+
+clearaction.forEach(element => {
+    element.addEventListener("click", (e) => {
+        if (e.target.matches('button')) { 
+        }
+        console.log(clear-key)
+    })
+})
+
+
+
