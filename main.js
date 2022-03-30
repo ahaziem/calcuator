bf6f87b625b0f07becad538d6f8609547c31c41e
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-03-30 15:01:11 +0100

    made changes

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-document.getElementById(number).addEventListener("click", () => console.log ('clicked!'));
-const numberbuttons = document.getElementsByClassName('number'); for (const button of buttons) {
-    button.addEventListener ('click', () => console.log ('clicked!')); }
+// select the button in the caluator
+const numberKeys = document.querySelectorAll(".number");
+const actionKey = document.querySelectorAll(".button-action");
+const clearaction = document.querySelectorAll(".button-action-clear");
+console.log(numberKeys)
+console.log("hello")
+//Make it interactive so does 
+
+// document.getElementById(number).addEventListener("click", () => console.log ('clicked!'));
+numberKeys.forEach( element => {
+
+    element.addEventListener( "click", (e) => {
+        console.log( e.target );
+    });
+
+});
