44425b6706108054bf96296e549d9121bde904b6
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-04-05 10:53:34 +0100

    made changes

--- main.js
+++ main.js
@@ -2,17 +2,22 @@
 const numberKeys = document.querySelectorAll(".number");
 const demicalkey = document.querySelectorAll(".number-decimal");
 const operationKey = document.querySelectorAll(".button-action");
-const clearAction = document.querySelectorAll(".button-action-clear");
+const clearAction = document.querySelector(".button-action-clear");
+const calculatorDisplay = document.querySelector(".calcuator-display")
 // console.log(numberKeys)
-
+console.log(clearAction)
 
 //Make it interactive for each list of the numbers in the calculator
-// document.getElementById(number).addEventListener("click", () => console.log ('clicked!'));
+
 numberKeys.forEach(element => {
     element.addEventListener("click", (e) => {
         if (e.target.matches('button')) {
         }
+// display the action of the calculator by pressing the button
         console.log(e.target.innerHTML)
+        console.log( calculatorDisplay.innerHTML );
+        const newValue = calculatorDisplay.innerHTML + e.target.innerHTML;
+        calculatorDisplay.innerHTML = newValue;
     })
 })
 
@@ -24,42 +29,129 @@ demicalkey.forEach(element => {
     })
 })
 
-operationKey.forEach(element => {
+// operationKey.forEach(element => {
+//     element.addEventListener("click", (e) => {
+//         if (e.target.matches('button')) {
+//         }
+//         console.log('operation-key')
+//     })
+// })
+
+// clearAction.forEach(element => {
+//     element.addEventListener("click", (e) => {
+//         if (e.target.matches('button')) {
+//         }
+//         console.log('clear-key')
+//     })
+// })
+
+
+// press on the clear button and the display is cleared
+clearAction.addEventListener("click", () => {
+    calculatorDisplay.innerHTML = "";
+  });
+
+    // press on the operation key and the display is cleared
+
+//   operationKey.forEach(element => {
+//     element.addEventListener("click", () => {
+//         calculatorDisplay.innerHTML = "+"||"-"||"x"||"÷"|| "="
+// });
+
+// display the action of the calculator by pressing the button (the operation e.g. x)
+
+  operationKey.forEach(element => {
     element.addEventListener("click", (e) => {
         if (e.target.matches('button')) {
         }
-        console.log('operation-key')
+        console.log(e.target.innerHTML)
+        console.log( calculatorDisplay.innerHTML );
+        const newoperation = calculatorDisplay.innerHTML + e.target.innerHTML;
+        calculatorDisplay.innerHTML = newoperation;
     })
 })
+// display the action of the calculator by pressing the demical button
 
-clearAction.forEach(element => {
+demicalkey.forEach(element => {
     element.addEventListener("click", (e) => {
         if (e.target.matches('button')) {
         }
-        console.log('clear-key')
+        console.log(e.target.innerHTML)
+        console.log(calculatorDisplay.innerHTML );
+        const newdemical = calculatorDisplay.innerHTML + e.target.innerHTML;
+        calculatorDisplay.innerHTML = newdemical;
     })
 })
+// press a number then store the number and press the operation key (+/- etc)
+//press another number to produce the data
 
-// display the action of the calculator by pressing the button
-const calculatorDisplay = document.querySelectorAll(".calcuator-display")
+// operationKey.forEach(operator => {
+//     operator.addEventListener("click", () => {
+//      console.log(operatorKey.innerHTML);
+// if (calcuator-display.innerHTML != '0') {
+// const numberStore = numberKeys.innerHTML + operationKey.innerHTML }
+// else ( numberKeys.innerHTML = "NULL")
+// }
 
-calculatorDisplay.addEventListener('click', (event) => {
-    const target = e.target;
+// operationKey.forEach(operator => {
+//     operator.addEventListener("click", () => {
+//      console.log(operator.innerHTML);
+//      if (display.innerHTML != '0') {
+//        storedNumber  += operator.innerHTML
+//        console.log(storedNumber)
+//      } else {
+//        number.innerHTML = "ERROR";
+//      }
+//      display.innerHTML = "";
+//    })
+//    })
 
-    if (!e.target.matches('button')) {
-        return;
-    }
-    if (e.target.classLift.contains('number')) {
-        return;
-    }
-    if (e.target.classLift.contains('number-decimal')) {
-        return;
-    }
-    if (e.target.classLift.contains('button-action')) {
-        return;
-    }
-    if (e.target.classLift.contains('button-action-clear')) {
-        return;
-    }
-console.log (calculatorDisplay)
-});
+// operationKey.forEach(operator => {
+//operationKey.addEventListener("click",(number) => {
+//   switch {
+//         case ""
+//     }
+//     calculatorDisplay.innerHTML =
+
+// numberKeys.forEach(element => {
+//     element.addEventListener("click", (e) => {
+//         if (e.target.matches('button')) {
+//         }
+// // display the action of the calculator by pressing the button
+//         console.log(e.target.innerHTML)
+//         console.log( calculatorDisplay.innerHTML );
+//         const newValue = calculatorDisplay.innerHTML + e.target.innerHTML;
+//         calculatorDisplay.innerHTML = newValue;
+//     })
+// })
+
+// const equal = numberKeys.innerHTML + operationKey.innerHTML
+// const firstNumber = numberKeys
+// const secondNumber = numberKeys
+
+// numberKeys.forEach(element => {
+//         element.addEventListener("click", (e) => {       
+// case '-':
+//     return console.log(calculatorDisplay = firstNumber - secondNumber)
+//     case '+':
+//         return console.log(calculatorDisplay = firstNumber + secondNumber)
+//     case '*':
+//         return console.log(calculatorDisplay = firstNumber * secondNumber)
+//     case '/':
+//     return console.log(calculatorDisplay = firstNumber / secondNumber)
+//     case '%':
+//     return console.log(calculatorDisplay = firstNumber * 0.01) || console.log(calculatorDisplay = secondNumber * 0.01)
+//             }
+
+            // const calculatoroperation = numberKeys.forEach(element => {
+            //             element.addEventListener("click", (numberKeys, operationKey) => {
+            //     if (operationKey === '+') return calculatorDisplay = numberKeys + numberKeys;
+            //     if (operationKey === '-') return calculatorDisplay = numberKeys - numberKeys;
+            //     if (operationKey === '*') return calculatorDisplay = numberKeys * numberKeys;
+            //     if (operationKey === '/') return calculatorDisplay = numberKeys === 0 ? "Can't divide by 0!" : numberKeys / numberKeys;
+            
+            //     console.log(calculatorDisplay)
+            
+            // }
+
+                    
